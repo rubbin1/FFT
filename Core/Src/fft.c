5bb21332b4ee7ebd337ac12f142cbc7ef7cf976c
rubbin <18943043570@163.com>
2026-04-26 00:37:40 +0800

    1.2版，在汉宁窗的基础上，增加使用了三点抛物线差值法，误差在允许的范围内了

--- Core/Src/fft.c
+++ Core/Src/fft.c
@@ -39,18 +39,27 @@ void fft_process()
 {
      //1.FFT计算
      arm_cfft_f32(&scfft, FFT_buffer, 0, 1);
-     //3.计算复数幅度
+     //2.计算复数幅度
      arm_cmplx_mag_f32(FFT_buffer, FFT_buffer, FFT_LEN);
-     //4.幅值校准
-     for (int i = 0; i < FFT_LEN; i++)
+     //3.幅值校准
+     for (int i = 0; i < FFT_LEN / 2; i++)
      {
-         FFT_buffer[i] = FFT_buffer[i] * 4.0f / FFT_LEN;
+         FFT_buffer[i] = FFT_buffer[i] * 4.0f / (float)FFT_LEN;
      }
-     //5.找基波，求基波幅值、基波频率
-     arm_max_f32(FFT_buffer, FFT_LEN / 2, &FFT_mag_max, &FFT_mag_max_index);
-     //
-     FFT_freq = (float)(FFT_mag_max_index) * sample_rate / (float)FFT_LEN;
-     FFT_ampl = FFT_mag_max;
+     //4.搜索峰值
+     float max_val;
+     uint32_t max_pos;
+     arm_max_f32(&FFT_buffer[1], FFT_LEN/2 - 1, &max_val, &max_pos);
+     max_pos += 1;   // 恢复真正的索引
+    //5.三点抛物线插值，消除栅栏效应
+     float y1 = FFT_buffer[max_pos - 1];
+     float y2 = FFT_buffer[max_pos];
+     float y3 = FFT_buffer[max_pos + 1];
+
+     float delta = 0.5f * (y1 - y3) / (y1 - 2.0f * y2 + y3);
+
+     FFT_freq = (max_pos + delta) * sample_rate / (float)FFT_LEN;
+     FFT_ampl = y2 - 0.25f * (y1 - y3) * delta;
      printf("FFT_freq = %.4f\n", FFT_freq);
      printf("FFT_ampl = %.4f\n", FFT_ampl);
 }
